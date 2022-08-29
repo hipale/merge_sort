2d08cced24ab7eeebcfd5ee449df4e21fe3ccd1b
hipale <hhemmhilb@gmail.com>
2022-08-29 19:06:44 +0200

    Add functions that split the array

--- main.rb
+++ main.rb
@@ -1,26 +1,12 @@
-def merge_sort(arr, merge = false)
-  print arr
-  0.upto(arr.length - 2) do |num| 
-    if arr.none?  { arr[num] > arr[num + 1]} && arr.length != 4
-      return arr
-    elsif merge == true
-      new_arr = []
-      first_half = arr[1]
-      two_half = arr[1]
-      until first_half = [] && two_half = []
-        if first_half.first < two_half.first
-           new_arr.push(first_half.first)
-           puts "a"
-        else 
-          new_arr.push(two_half.first)
-        end
-        merge_sort(new_arr)
-      end 
-    elsif arr.length <= 2
-      new_arr = [merge_sort(arr.slice(arr[0], arr.length / 2)), merge_sort(arr.slice(arr.length / 2 - 1, arr.last))]
-      merge_sort(new_arr, true)
-    end
-  end
+def merge_sort(arr)
+  if arr.length <= 2
+    arr
+  else 
+    [
+    merge_sort(arr.slice(0, (arr.length.to_f / 2).ceil)), 
+    merge_sort(arr.slice((arr.length.to_f / 2).ceil, arr.length - 1))
+    ]
+  end   
 end
 
-    puts merge_sort([1, 3, 2, 6])
+    puts merge_sort([3, 1, 2, 4])
