d53f9676342460c99a1fcb46c9e2eb46e3b1ef0b
hipale <hhemmhilb@gmail.com>
2022-08-29 21:43:07 +0200

    Add new_arr,that takes the function and gives array of every array

--- main.rb
+++ main.rb
@@ -1,12 +1,18 @@
 def merge_sort(arr)
   if arr.length <= 2
-    arr
+    if arr[0] > arr[-1]
+      arr[0], arr[-1] = arr[-1], arr[0]
+    else 
+      arr
+    end
+    print arr
   else 
-    [
-    merge_sort(arr.slice(0, (arr.length.to_f / 2).ceil)), 
-    merge_sort(arr.slice((arr.length.to_f / 2).ceil, arr.length - 1))
+    new_arr = [
+    (arr.slice(0, (arr.length.to_f / 2).ceil)), 
+    (arr.slice((arr.length.to_f / 2).ceil, arr.length - 1))
     ]
+    merge_sort(new_arr)
   end   
 end
 
-    puts merge_sort([3, 1, 2, 4])
+    puts merge_sort([3, 1, 4, 8, 55, 14])
