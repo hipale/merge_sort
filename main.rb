d8af0de5f8785e64039b47584be81870ea53ae96
hipale <hhemmhilb@gmail.com>
2022-08-31 18:15:38 +0200

    Try other ideas of sort array Delete mis of it

--- main.rb
+++ main.rb
@@ -1,18 +1,12 @@
 def merge_sort(arr)
   if arr.length <= 2
-    if arr[0] > arr[-1]
-      arr[0], arr[-1] = arr[-1], arr[0]
-    else 
-      arr
-    end
     print arr
   else 
-    new_arr = [
-    (arr.slice(0, (arr.length.to_f / 2).ceil)), 
-    (arr.slice((arr.length.to_f / 2).ceil, arr.length - 1))
-    ]
-    merge_sort(new_arr)
+    right_side = arr.slice(0, (arr.length.to_f / 2).ceil)
+    left_side = arr.slice((arr.length.to_f / 2).ceil, arr.length - 1)
+    merge_sort(right_side.sort)
+    merge_sort(left_side.sort)
   end   
 end
 
-    puts merge_sort([3, 1, 4, 8, 55, 14])
+    puts merge_sort([3, 1, 4, 2, 5, 4])
